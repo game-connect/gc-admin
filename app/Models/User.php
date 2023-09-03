6494d53621c5f82688eeb2b401c4760b5fb1c163
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-09-03 17:34:32 +0900

    ユーザー登録時にgame dbにも企業情報を追加する処理を追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'admin_user_key',
+        'token',
         'name',
         'email',
         'password',
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('admin_user_key')->unique();
+            $table->string('token');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
