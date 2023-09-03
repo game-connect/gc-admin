5b6bfe0a2c713e3afc986f571b8f4c68fe941432
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-09-03 17:04:13 +0900

    ユーザー登録時にKeyを生成する処理を追加

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,6 +65,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
+            'admin_user_key' => uniqid("gcadmin"),
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
